b4c1bbf2b7f8a319bbd186ff4d09d635703b2178
Jared White <jared@jaredwhite.com>
2020-06-30 12:33:27 -0700

    Refactor queries DSL

--- bridgetown/plugins/site_builder.rb
+++ bridgetown/plugins/site_builder.rb
@@ -1,10 +1,30 @@
 class SiteBuilder < Bridgetown::Builder
-  # write builders which subclass SiteBuilder in plugins/builder
+  class Queries < OpenStruct; end
 
-  # Construct a GraphQL client pointing to the graqhql_endpoint config variable
-  def graphql(graph_name)
-    client = Graphlient::Client.new(config[:graphql_endpoint])
-    client.execute(send(:"#{graph_name}_graph").to_s).data.send(graph_name)
+  class << self
+    attr_accessor :graphql_queries
+
+    def graphql(graph_name, &block)
+      self.graphql_queries ||= Queries.new
+      graphql_queries.send("#{graph_name}=", block)
+    end
+  end
+
+  def queries
+    return self.class.graphql_queries if @_executed_queries
+
+    self.class.graphql_queries.each_pair do |graph_name, block|
+      client = Graphlient::Client.new(config[:graphql_endpoint])
+      graph_dsl = Graphlient::Query.new(&block)
+
+      self.class.graphql_queries.send(
+        "#{graph_name}=",
+        client.execute(graph_dsl.to_s).data.send(graph_name)
+      )
+    end
+
+    @_executed_queries = true
+    self.class.graphql_queries
   end
 end
 
